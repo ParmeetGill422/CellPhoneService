d7d0efa1bb4c7d94287638c7c3d1ece0241947b4
ParmeetGill422 <parmeetgill422@gmail.com>
2025-04-17 17:31:30 -0700

    Initial commit

---
+++ src/CellPhone.java
@@ -0,0 +1,16 @@
+public class Main {
+    int serialNumber;
+    String model;
+    String carrier;
+    String phoneNumber;
+    String owner;
+
+    // Default constructor
+    public Main() {
+        serialNumber = 0;
+        model = "";
+        carrier = "";
+        phoneNumber = "";
+        owner = "";
+    }
+}
